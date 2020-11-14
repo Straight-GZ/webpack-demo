dcb5bf60fa2eca7a275e452f1f2486bca84df12c
Straight <gengzhi1993@163.com>
2020-11-14 15:56:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import x from "./x.js"
 import png from "./assets/1.png"
 
 const div = document.getElementById("app")
-console.log("hi")
 // div.innerHTML = `
 //     <img src='${png}'>
 // `
@@ -15,7 +14,9 @@ button.onclick = () => {
       const fn = module.default
       fn()
     },
-    () => {}
+    () => {
+      console.log("加载错误")
+    }
   )
 }
 div.appendChild(button)
